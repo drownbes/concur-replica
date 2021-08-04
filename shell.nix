6c084c56c9a1f08ef5b0fb6ba79c5d621d926fa6
drownbes <drownbes@gmail.com>
2021-08-04 23:09:35 +0300

    works

---
+++ shell.nix
@@ -0,0 +1,17 @@
+with (import ./haskell.nix);
+let replica = fetchgit {
+  url = "git@github.com:drownbes/replica.git";
+  rev = "5ad6f2e965666a3fc5a37c4a84531e3b10361939";
+};
+in project.shellFor {
+  withHoogle = true;
+  buildInputs = [
+    pkgs.haskellPackages.hpack
+    pkgs.zlib
+  ];
+  tools = {
+    cabal = "3.2.0.0";
+    hlint = "3.2.7";
+    haskell-language-server = "latest";
+  };
+}
